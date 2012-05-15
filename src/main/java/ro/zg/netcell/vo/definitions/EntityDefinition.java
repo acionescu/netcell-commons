23bbd27308c48d558e3cbdf9f6a810c8d47f0d8b
acionescu <adrian.ionescu.consulting@gmail.com>
2012-05-15 17:55:42 +0300

    added compareTo

--- src/main/java/ro/zg/netcell/vo/definitions/EntityDefinition.java
+++ src/main/java/ro/zg/netcell/vo/definitions/EntityDefinition.java
@@ -17,87 +17,101 @@ package ro.zg.netcell.vo.definitions;
 
 import java.io.Serializable;
 
-public class EntityDefinition implements Serializable{
+public class EntityDefinition implements Serializable,
+	Comparable<EntityDefinition> {
     private static final long serialVersionUID = 5946883492282248859L;
-    
+
     private String id;
     private String description;
     private String type;
     private EntityType entityType;
-    
-    public boolean validate(){
-	//TODO : implement this
+
+    public boolean validate() {
+	// TODO : implement this
 	return true;
     }
-    
+
     /**
      * @return the id
      */
     public String getId() {
-        return id;
+	return id;
     }
+
     /**
      * @return the description
      */
     public String getDescription() {
-        return description;
+	return description;
     }
+
     /**
      * @return the type
      */
     public String getType() {
-        return type;
+	return type;
     }
+
     /**
-     * @param id the id to set
+     * @param id
+     *            the id to set
      */
     public void setId(String id) {
-        this.id = id;
+	this.id = id;
     }
+
     /**
-     * @param description the description to set
+     * @param description
+     *            the description to set
      */
     public void setDescription(String description) {
-        this.description = description;
+	this.description = description;
     }
+
     /**
-     * @param type the type to set
+     * @param type
+     *            the type to set
      */
     public void setType(String type) {
-        this.type = type;
-        this.entityType = EntityType.getEntityType(type);
+	this.type = type;
+	this.entityType = EntityType.getEntityType(type);
     }
-    
-    
-    
+
     /**
      * @return the entityType
      */
     public EntityType getEntityType() {
-        return entityType;
+	return entityType;
     }
 
     /**
-     * @param entityType the entityType to set
+     * @param entityType
+     *            the entityType to set
      */
     public void setEntityType(EntityType entityType) {
-        this.entityType = entityType;
-        this.type = entityType.getFullType();
+	this.entityType = entityType;
+	this.type = entityType.getFullType();
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#hashCode()
      */
     @Override
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
-	result = prime * result + ((description == null) ? 0 : description.hashCode());
+	result = prime * result
+		+ ((description == null) ? 0 : description.hashCode());
 	result = prime * result + ((id == null) ? 0 : id.hashCode());
 	result = prime * result + ((type == null) ? 0 : type.hashCode());
 	return result;
     }
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#equals(java.lang.Object)
      */
     @Override
@@ -126,5 +140,19 @@ public class EntityDefinition implements Serializable{
 	    return false;
 	return true;
     }
-    
+
+    public int compareTo(EntityDefinition arg0) {
+	if (arg0 == null) {
+	    return -1;
+	}
+	if (id != null) {
+	    return id.compareTo(arg0.id);
+	}
+
+	if (arg0.id == null) {
+	    return 0;
+	}
+	return 1;
+    }
+
 }
